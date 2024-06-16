5ed50ec25e52cb62d56667ce25b21a3fc4387496
Nirav Parekh <niruparekh09@gmail.com>
2024-06-16 10:25:03 +0530

    Created Product Microservices for kafka

---
+++ Kafka/ProductsMicroservice/src/test/java/com/nrv/kafka/ProductsMicroservice/ProductsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrv.kafka.ProductsMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
